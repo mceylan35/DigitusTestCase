d283200db8afffda53949cb2a02d8fb2517ae59c
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-04 21:05:48 +0300

    EmailService eklendi

---
+++ WebAPP/DigitusTestCase.WebAPP/Services/EmailService/EmailConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitusTestCase.WebAPP.Services.EmailService
+{
+    public class EmailConfiguration
+    {
+        public string Secret { get; set; }
+        public int RefreshTokenTTL { get; set; }
+        public string EmailFrom { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; }
+        public string SmtpPass { get; set; }
+    }
+}
---
+++ WebAPP/DigitusTestCase.WebAPP/Services/EmailService/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text; 
+namespace DigitusTestCase.WebAPP.Services.EmailService
+{
+    public class EmailService: IEmailService
+    {
+        private readonly EmailConfiguration _appSettings;
+
+        public EmailService(IOptions<EmailConfiguration> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        public void Send(string to, string subject, string html, string from = null)
+        {
+            // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from ?? _appSettings.EmailFrom));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(_appSettings.SmtpHost, _appSettings.SmtpPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_appSettings.SmtpUser, _appSettings.SmtpPass);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
