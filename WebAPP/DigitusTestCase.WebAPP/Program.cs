5740bc22ad109bec83a32a7ae15d5781dae25dc2
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-08 02:20:39 +0300

    addIdentity eklendi. mongo db configure edildi signalr yapılandırıldı

--- WebAPP/DigitusTestCase.WebAPP/Program.cs
+++ WebAPP/DigitusTestCase.WebAPP/Program.cs
@@ -1,17 +1,51 @@
+using DigitusTestCase.WebAPP.Hubs;
 using DigitusTestCase.WebAPP.Models;
+using DigitusTestCase.WebAPP.Services.EmailService;
+using DigitusTestCase.WebAPP.Services.UserService;
 using DigitusTestCase.WebAPP.Settings;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Configuration; 
+using static Org.BouncyCastle.Math.EC.ECCurve;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var services = builder.Services;
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+services.AddControllersWithViews();
 #region MongoDb Settings
 var setting = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
-   .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>(setting.ConnectionString, setting.Name);
+services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Tokens.PasswordResetTokenProvider = "passwordReset";
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Password.RequiredLength = 0;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcçdefghiýjklmnoöpqrsþtuüvwxyzABCÇDEFGHIÝJKLMNOÖPQRSÞTUÜVWXYZ0123456789-._@+'#!/^%{}*";
+
+
+}).AddDefaultTokenProviders().AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>(setting.ConnectionString, setting.Name);
+
+services.Configure<MongoDbConfig>(options =>
+{
+    options.ConnectionString = builder.Configuration.GetSection("MongoConnection:ConnectionString").Value;
+    options.Host = builder.Configuration.GetSection("MongoConnection:Database").Value;
+
+});
 #endregion
+services.AddScoped<IEmailService, EmailService>();
+services.AddScoped<UserManager<ApplicationUser>>();
+services.AddScoped<UserService>();
+services.AddSignalR(); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +55,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ 
+app.MapHub<OnlineCountHub>("/onlinecount");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
  
