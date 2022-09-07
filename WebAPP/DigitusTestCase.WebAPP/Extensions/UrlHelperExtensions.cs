2598bb1ffd78992149585b7bf50868001dc1df2b
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-08 02:15:18 +0300

    resetpassword link url helper extension eklendi

---
+++ WebAPP/DigitusTestCase.WebAPP/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DigitusTestCase.WebAPP.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitusTestCase.WebAPP.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ResetPasswordCallBackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AuthenticationController.ResetPassword),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+    }
+}
