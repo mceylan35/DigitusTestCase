e61207745ab11cbf3ff003841f1d382244eb2733
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-09 00:54:32 +0300

    userservice güncellendi

--- WebAPP/DigitusTestCase.WebAPP/Services/UserService/UserService.cs
+++ WebAPP/DigitusTestCase.WebAPP/Services/UserService/UserService.cs
@@ -13,25 +13,28 @@ namespace DigitusTestCase.WebAPP.Services.UserService
 
         public double CompleteLoginAverage(DateTime dateTime)
         {
-            var userList = this.Users.Where(c => c.EmailConfirmed == true && c.CreatedOn.Date == dateTime.Date);
+        
+            var userList = this.Users.Where(c => c.EmailConfirmed == true);// && c.CreatedOn.Date == dateTime.Date);
             List<double> dateBetween = new List<double>();
             foreach (var item in userList)
             {
-                TimeSpan timeSpan = (DateTime)item.SendVerificationCodeDate - item.CreatedOn;
+                TimeSpan timeSpan = (DateTime)item.MailConfirmationDate - item.CreatedOn;
 
                 dateBetween.Add(timeSpan.TotalSeconds);
             }
             return dateBetween.Average();
         }
-        public int SentVerificationCodeButDidNotRegisterAfter(string data)
+        public int SentVerificationCodeButDidNotRegisterAfter(DateTime date)
         {
-            var count = this.Users.Count(i => i.EmailConfirmed == false && i.CreatedOn.AddDays(1) > DateTime.UtcNow);
+          
+            var count = this.Users.Count(i => i.EmailConfirmed == false && date > DateTime.Now);
             return count;
         }
-        public int ListingByTimeRange(string date)
+        public int ListingByTimeRange(DateTime date)
         {
-            var oneDate = DateTime.UtcNow.AddDays(-1);
-            return this.Users.Count(i => i.CreatedOn > oneDate && i.CreatedOn < DateTime.UtcNow);
+           
+            var oneDate = DateTime.Now.AddDays(-1);
+            return this.Users.Count(i => i.CreatedOn > date && i.CreatedOn < DateTime.Now);
             
         }
     }
