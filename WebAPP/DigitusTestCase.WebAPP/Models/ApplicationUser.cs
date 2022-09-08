642c3e621d50f10555e26246cfff00a4320d8fb6
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-09 00:56:19 +0300

    models güncellendi

--- WebAPP/DigitusTestCase.WebAPP/Models/ApplicationUser.cs
+++ WebAPP/DigitusTestCase.WebAPP/Models/ApplicationUser.cs
@@ -7,7 +7,7 @@ namespace DigitusTestCase.WebAPP.Models
     public class ApplicationUser : MongoIdentityUser<Guid>
     {
         public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime? SendVerificationCodeDate { get; set; }
+        public string Surname { get; set; } 
+        public DateTime? MailConfirmationDate { get; set; }
     }
 }
--- WebAPP/DigitusTestCase.WebAPP/Models/User.cs
+++ WebAPP/DigitusTestCase.WebAPP/Models/User.cs
@@ -4,19 +4,14 @@ namespace DigitusTestCase.WebAPP.Models
 {
     public class User
     {
-        //Adını, soyadını, e-posta adresini ve şifresini kullanarak
-      //  [Required]
-        public string Name { get; set; }
-      //  [Required]
-        public string SurName { get; set; }
-       // [Required]
-        //[EmailAddress(ErrorMessage = "Invalid Email")]
+    
+        public string Name { get; set; } 
+        public string SurName { get; set; } 
         public string Email { get; set; }
 
-       // [Required]
+       
         public string Password { get; set; }
-       // public string MailCode { get; set; }
-       // public bool IsActive { get; set; }
+         public string UserName { get; set; } 
 
     }
 }
