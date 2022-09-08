1d03602e294d49c22d59a17a7aaec467c35d1098
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-09 00:57:04 +0300

    mongodbcontext eklendi

---
+++ WebAPP/DigitusTestCase.WebAPP/DbContext/MongoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace DigitusTestCase.WebAPP.DbContext
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+        public IMongoCollection<TEntity> GetCollection<TEntity>()
+        {
+            return _database.GetCollection<TEntity>(typeof(TEntity).Name.Trim());
+        }
+        public IMongoDatabase GetDatabase()
+        {
+            return _database;
+        }
+
+    }
+}
