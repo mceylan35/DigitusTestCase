9ddf7a6aeb8cc647b6a8953ef7cf49d73644567a
mceylan35 <mehmetceylan2696@gmail.com>
2022-09-08 03:24:50 +0300

    signal R ile online kullanıcı sayısı rapor sayfasına eklendi

---
+++ WebAPP/DigitusTestCase.WebAPP/Hubs/OnlineCountHub.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DigitusTestCase.WebAPP.Hubs
+{
+    //[Authorize]
+    public class OnlineCountHub : Hub
+    {
+        private static readonly Dictionary<string, int> onlineUsers = new Dictionary<string, int>();
+        private static int Count = 0;
+        public Task<ConnectionOpenedResult> ConnectionOpened(string userId)
+        {
+            var joined = false;
+            lock (onlineUsers)
+            {
+                if (onlineUsers.ContainsKey(userId))
+                {
+                    onlineUsers[userId] += 1;
+                }
+                else
+                {
+                    onlineUsers.Add(userId, 1);
+                    joined = true;
+                }
+            }
+            return Task.FromResult(new ConnectionOpenedResult { UserJoined = joined });
+        }
+        public Task<ConnectionClosedResult> ConnectionClosed(string userId)
+        {
+            var left = false;
+            lock (onlineUsers)
+            {
+                if (onlineUsers.ContainsKey(userId))
+                {
+                    onlineUsers[userId] -= 1;
+                    if (onlineUsers[userId] <= 0)
+                    {
+                        onlineUsers.Remove(userId);
+                        left = true;
+                    }
+                }
+            }
+
+            return Task.FromResult(new ConnectionClosedResult { UserLeft = left });
+        }
+        public int GetOnlineUsersCount()
+        {
+            lock (onlineUsers)
+            {
+                return onlineUsers.Count;
+            }
+        }
+        public override Task OnConnectedAsync()
+        {
+
+            ConnectionOpened(Context.ConnectionId);
+            base.OnConnectedAsync();
+            Clients.All.SendAsync("updateCount", GetOnlineUsersCount());
+            return Task.CompletedTask;
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+
+            ConnectionClosed(Context.ConnectionId);
+            base.OnDisconnectedAsync(exception);
+            Clients.All.SendAsync("updateCount", GetOnlineUsersCount());
+            return Task.CompletedTask;
+        }
+    }
+    public class ConnectionOpenedResult
+    {
+        public bool UserJoined { get; set; }
+    }
+
+    public class ConnectionClosedResult
+    {
+        public bool UserLeft { get; set; }
+    }
+}
+ 
---
+++ WebAPP/DigitusTestCase.WebAPP/wwwroot/js/onlineCount.js
@@ -0,0 +1,21 @@
+﻿let onlineCount = document.querySelector('span.online-count');
+let updateCountCallback = function (message) {
+    if (!message) return;
+    console.log('updateCount = ' + message);
+    if (onlineCount) onlineCount.innerText = message;
+};
+
+function onConnectionError(error) {
+    if (error && error.message) console.error(error.message);
+}
+
+let countConnection = new signalR.HubConnectionBuilder().withUrl('/onlinecount').build();
+countConnection.on('updateCount', updateCountCallback);
+countConnection.onclose(onConnectionError);
+countConnection.start()
+    .then(function () {
+        console.log('OnlineCount Connected');
+    })
+    .catch(function (error) {
+        console.error(error.message);
+    });
